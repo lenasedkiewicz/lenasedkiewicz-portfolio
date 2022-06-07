cb2005d146ecee1b59038fba05163e77d374edf4
Lena <lena@onely.com>
2022-06-07 12:29:53 +0200

    Fix about.js page; add Link

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,3 +1,17 @@
-import React from "react";
+import React from "react"
+import { Link } from "gatsby"
 
-fac
+const AboutPage = () => {
+    return (
+        <main>
+            <Link to="/">
+                Back to homepage
+            </Link>
+            <title>About me</title>
+            <h1>Hi! I'm Lena!</h1>
+            <p>I'm psychologist and a programmer, and I love crochet and other handcraft arts!</p>
+        </main>
+    )
+}
+
+export default AboutPage
