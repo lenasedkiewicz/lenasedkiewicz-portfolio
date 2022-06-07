f2fb9b79261fd4faf6d002530b794919125e116b
Lena <lena@onely.com>
2022-06-07 11:46:24 +0200

    Changing index.js page content

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,2 +1,13 @@
-import React from 'react';
+import React from "react";
 
+const HomePage = () => {
+    return (
+        <main>
+            <title>Handcraft Heaven</title>
+            <h1>Crochet, half-cross stitch and decoupage</h1>
+            <p>Welcome to my site! I share here photos of my work of arts.</p>
+        </main>
+    )
+}
+
+export default HomePage
