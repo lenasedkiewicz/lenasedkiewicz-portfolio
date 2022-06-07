5b3f4ace6fbfe7604ee2d88dca2d8eb328896fd9
Lena <lena@onely.com>
2022-06-07 12:30:14 +0200

    Add Link component to index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React from "react"
+import { Link } from "gatsby"
 
 const HomePage = () => {
     return (
         <main>
+            <Link to="/about">
+                About me
+            </Link>
             <title>Handcraft Heaven</title>
             <h1>Crochet, half-cross stitch and decoupage</h1>
             <p>Welcome to my site! I share here photos of my work of arts.</p>
