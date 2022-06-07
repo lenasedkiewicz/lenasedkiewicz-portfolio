a6c9dff3035357d580c87bad4577d5e70bb360e5
lenasedkiewicz <93651753+lenasedkiewicz@users.noreply.github.com>
2022-06-07 13:54:52 +0200

    Fix filename typo

---
+++ src/pages/about.js
@@ -0,0 +1,17 @@
+import React from "react"
+import Layout from "../components/layout.js"
+
+const AboutPage = () => {
+    return (
+        <main>
+            <Layout
+                pageTitle="About me"
+                pageHeading="Hi! I'm Lena!"                
+            >
+                I'm psychologist and a programmer, and I love crochet and other handcraft arts!
+            </Layout>
+        </main>
+    )
+}
+
+export default AboutPage
