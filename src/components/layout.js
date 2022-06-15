f674cf8174432f7bc9b07162ef792c00be73cfa4
Lena <lena@onely.com>
2022-06-15 12:22:58 +0200

    Fix siteMetadata Error

--- src/components/layout.js
+++ src/components/layout.js
@@ -13,36 +13,41 @@ import {
 } from "./layout.module.css"
 
 const Layout = ({ pageTitle, pageHeading, children }) => {
-    const data = StaticQuery(graphql`
-    query PageTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `);
 
     return (
-        <main className={container} >
-            <title>{pageTitle} | {data.site.siteMetadata.title}</title>
-            <nav>
-                <ul className={navLinks}>
-                    <li className={navLinkItem}>
-                        <Link to="/" className={navLinkText} >Home</Link>
-                    </li>
-                    <li className={navLinkItem}>
-                        <Link to="/about" className={navLinkText} >About</Link>
-                    </li>
-                    <li className={navLinkItem}>
-                        <Link to="/gallery" className={navLinkText} >Gallery</Link>
-                    </li>
-                </ul>
-            </nav>
-            <h1 className={heading}>{pageHeading}</h1>
-            {children}
-        </main>
-    )
-}
+        <StaticQuery
+            query={graphql`
+                query PageTitleQuery {
+                    site {
+                    siteMetadata {
+                        title
+                    }
+                    }
+                }
+            `}
+            render={data => (
+                <main className={container} >
+                    <title>{pageTitle} | {data.site.siteMetadata.title}</title>
+                    <header>{data.site.siteMetadata.title}</header>
+                    <nav>
+                        <ul className={navLinks}>
+                            <li className={navLinkItem}>
+                                <Link to="/" className={navLinkText} >Home</Link>
+                            </li>
+                            <li className={navLinkItem}>
+                                <Link to="/about" className={navLinkText} >About</Link>
+                            </li>
+                            <li className={navLinkItem}>
+                                <Link to="/gallery" className={navLinkText} >Gallery</Link>
+                            </li>
+                        </ul>
+                    </nav>
+                    <h1 className={heading}>{pageHeading}</h1>
+                    {children}
+                </main>
+            )}
+        />
+    );
+};
 
 export default Layout
