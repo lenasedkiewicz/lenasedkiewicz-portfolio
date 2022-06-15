a2d949b31ccd9a4d1bdf06b4965ab392c8408d93
Lena <lena@onely.com>
2022-06-15 16:42:43 +0200

    Add blog posts and query

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,12 +1,28 @@
 import React from "react"
 import Layout from "../components/layout"
+import { graphql } from "gatsby"
 
-const Blog = () => {
+const Blog = ( data ) => {
+  console.log(data);
   return (
       <Layout pageTitle="Blog" pageHeading="Blog">
-        <p>Hi!!</p>
+        <ul>
+          {data.allFile.nodes.map((node) => {
+            return<li>{node.name}</li>;
+          })}
+        </ul>
       </Layout>
   )
 };
+
+export const query = graphql`
+  query {
+    allFile(filter: {sourceInstanceName: {eq: "blog"}}) {
+      nodes {
+        name
+      }
+    }
+  }
+`;
 
 export default Blog
