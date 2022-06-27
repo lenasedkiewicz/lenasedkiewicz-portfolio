6e9893ee7a2e81750a4102ea9159fe1b96abc747
msedkiewicz <91282230+msedkiewicz@users.noreply.github.com>
2022-06-27 17:46:25 +0200

    Add 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function NotFound() {
+  return (
+    <Layout>
+        <div>
+        <h1>404</h1>
+        <p>Sorry, that page doesn't exist.</p>
+        </div>
+    </Layout>
+  )
+}
