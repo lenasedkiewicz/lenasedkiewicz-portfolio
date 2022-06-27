3c3a3f02c15b473195cbe2eaa1dd0964b5c3d6f0
Lena <lena@onely.com>
2022-06-27 20:41:03 +0200

    Add content to Projects page

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,10 +1,13 @@
 import React from "react";
-import ProjectsLayout from "../../components/ProjectsLayout"
+import ProjectsLayout from "../../components/ProjectsLayout";
+import * as styles from "../../styles/projects.module.css"
 
 export default function Projects() {
   return (
     <ProjectsLayout>
-      <section>
+      <section className={styles.portfolio}>
+        <h2>Portfolio</h2>
+        <h3>From created websites to learning projects.</h3>
         <div>Hi, it's project page</div>
       </section>
     </ProjectsLayout>
