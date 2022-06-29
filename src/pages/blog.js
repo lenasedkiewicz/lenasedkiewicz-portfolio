46288d4eb10794778a26f34e61297d92e61cc278
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2022-06-29 16:24:10 +0200

    Add blog posts content

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,24 +1,32 @@
 import React from "react";
 import Layout from "../components/layout";
 import { graphql } from "gatsby";
+import { MDXRenderer } from "gatsby-plugin-mdx";
 
 const Blog = ({ data }) => {
   return (
     <Layout pageTitle="Blog" pageHeading="Blog">
-      <ul>
-        {data?.allFile?.nodes.map((node) => {
-          return <li key={node.name}>{node.name}</li>;
-        })}
-      </ul>
+        {data?.allMdx?.nodes.map((node) => {
+        return <article key={node.id}>
+          <h2>{node.frontmatter.title}</h2>
+          <p>{node.frontmatter.date}</p>
+          <MDXRenderer>{node.body}</MDXRenderer>
+          </article>;
+      })}
     </Layout>
   );
 };
 
 export const query = graphql`
-  query {
-    allFile(filter: { sourceInstanceName: { eq: "blog" }}) {
+  query  {
+    allMdx {
       nodes {
-        name
+        frontmatter {
+          date(formatString: "dddd, Do MMMM YYYY")
+          title
+        }
+        id
+        body
       }
     }
   }
