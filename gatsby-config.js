e029844f2b5e901b5bb5e80dceaea632d6b774e3
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2022-06-30 18:33:47 +0200

    Add site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,4 +7,9 @@
 module.exports = {
   /* Your site config here */
   plugins: [],
+  siteMetadata: {
+    title: 'Lena Sedkiewicz',
+    description: 'Your WordPress Developer',
+    copyright: 'Copyright 2022 by Lena Sedkiewicz'
+  }
 }
