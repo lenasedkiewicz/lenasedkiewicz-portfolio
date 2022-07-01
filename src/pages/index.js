7e4f606dc41d2f0fcc0c74d78e6a494a4578cc40
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2022-07-01 20:01:39 +0200

    Change page query to static query

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,9 @@
-import { graphql, Link } from "gatsby";
+import { Link } from "gatsby";
 import React from "react";
 import Layout from "../components/Layout";
 import * as styles from "../styles/home.module.css";
 
-export default function Home({ data }) {
-  const { title, description } = data.site.siteMetadata
+export default function Home() {
   return (
     <Layout>
       <section className={styles.header}>
@@ -16,18 +15,6 @@ export default function Home({ data }) {
         <Link className={styles.btn} to="/projects">My Portfolio Projects</Link>
         <img src="/banner.png" alt="Site banner" style={{ maxWidth: '100%' }} />
       </section>
-      <p>{ title } - { description }</p>
     </Layout>
   )
 }
-
-export const query = graphql`
-  query SiteInfo {
-    site {
-      siteMetadata {
-        description
-        title
-      }
-    }
-  }
-`
