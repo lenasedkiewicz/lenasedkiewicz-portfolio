0445ef410fc677e44f06f6515846a0a2b0759789
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2022-07-04 20:30:42 +0200

    Render blog posts content

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -1,12 +1,29 @@
 import React from "react";
+import { graphql } from "gatsby";
+import { MDXRenderer } from "gatsby-plugin-mdx";
 import Layout from "../../components/layout";
 
- const BlogPost = () => {
+ const BlogPost = ({ data }) => {
     return (
-        <Layout pageTitle="BBBlog">
-            <div></div>
+        <Layout pageTitle={data.mdx.frontmatter.title} pageHeading={data.mdx.frontmatter.title}>
+            <p>{data.mdx.frontmatter.date}</p>
+            <MDXRenderer>
+                {data.mdx.body}
+            </MDXRenderer>
         </Layout>
     );
  };
+
+ export const query = graphql`
+ query ($id: String) {
+    mdx(id: {eq: $id}) {
+      frontmatter {
+        date(formatString: "dddd, Do MMMM YYYY")
+        title
+      }
+      body
+    }
+  }
+`;
 
  export default BlogPost
