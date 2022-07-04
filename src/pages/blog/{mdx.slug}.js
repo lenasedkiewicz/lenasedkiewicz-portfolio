7e625d74a39e2b6a2870cdbe4009763a9b6c4efb
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2022-07-04 20:06:46 +0200

    Add mdx.slug file, change routing

---
+++ src/pages/blog/{mdx.slug}.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../../components/layout";
+
+ const BlogPost = () => {
+    return (
+        <Layout pageTitle="BBBlog">
+            <div></div>
+        </Layout>
+    );
+ };
+
+ export default BlogPost
