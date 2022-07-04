ea703b147a8edd8e437af06d2e45617d5347407a
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2022-07-04 15:10:10 +0200

    Install gatsby image; add image to main page

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,19 +7,28 @@
 module.exports = {
   /* Your site config here */
   plugins: [
-    'gatsby-transformer-remark',
+    "gatsby-transformer-remark",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
     {
-    resolve: `gatsby-source-filesystem`,
-    options: {
-      name: `projects`,
-      path: `${__dirname}/src/projects/`,
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `projects`,
+        path: `${__dirname}/src/projects/`,
+      },
     },
-  },
-],
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+  ],
   siteMetadata: {
-    title: 'Lena Sedkiewicz',
-    description: 'Your WordPress Developer',
-    copyright: 'Copyright 2022 by Lena Sedkiewicz',
-    contact: 'contact@msedkiewicz.pl',
-  }
+    title: "Lena Sedkiewicz",
+    description: "Your WordPress Developer",
+    copyright: "Copyright 2022 by Lena Sedkiewicz",
+    contact: "contact@msedkiewicz.pl",
+  },
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
-import { Link } from "gatsby";
+import { graphql, Link } from "gatsby";
 import React from "react";
 import Layout from "../components/Layout";
 import * as styles from "../styles/home.module.css";
+import Img from "gatsby-image";
 
-export default function Home() {
+export default function Home({ data }) {
   return (
     <Layout>
       <section className={styles.header}>
@@ -12,9 +13,23 @@ export default function Home() {
           <h3>WordPress Developer</h3>
           <p>Creating and maintaining WordPress websites</p>
         </div>
-        <Link className={styles.btn} to="/projects">My Portfolio Projects</Link>
-        <img src="/banner.png" alt="Site banner" style={{ maxWidth: '100%' }} />
+        <Link className={styles.btn} to="/projects">
+          My Portfolio Projects
+        </Link>
+        <Img fluid={data.file.childImageSharp.fluid} />
       </section>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query Banner {
+    file(relativePath: { eq: "banner.png" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
