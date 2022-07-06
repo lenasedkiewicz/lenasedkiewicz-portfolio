ca0b85993f926cf553ff88ec86ddfb0e639f68b3
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2022-07-06 19:19:01 +0200

    Add image to blog post

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { graphql } from "gatsby";
 import { MDXRenderer } from "gatsby-plugin-mdx";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import Layout from "../../components/layout";
 
  const BlogPost = ({ data }) => {
+  const image = getImage(data.mdx.frontmatter.hero_image);
     return (
         <Layout pageTitle={data.mdx.frontmatter.title} pageHeading={data.mdx.frontmatter.title}>
             <p>{data.mdx.frontmatter.date}</p>
+            <GatsbyImage image={image} alt={data.mdx.frontmatter.hero_image_alt} />
+            <p><a href={data.mdx.frontmatter.hero_image_credit_link} target="_blank" rel="noreferrer">{data.mdx.frontmatter.hero_image_credit_text}</a></p>
             <MDXRenderer>
                 {data.mdx.body}
             </MDXRenderer>
@@ -18,8 +22,16 @@ import Layout from "../../components/layout";
  query ($id: String) {
     mdx(id: {eq: $id}) {
       frontmatter {
-        date(formatString: "dddd, Do MMMM YYYY")
+        date
         title
+        hero_image_alt
+        hero_image_credit_link
+        hero_image_credit_text
+        hero_image {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
       }
       body
     }
