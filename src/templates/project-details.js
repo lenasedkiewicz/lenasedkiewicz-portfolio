b4c29e8e3d384131fc733eab27bc4f58491504eb
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2022-07-06 14:19:01 +0200

    Add data to project post

--- src/templates/project-details.js
+++ src/templates/project-details.js
@@ -1,19 +1,41 @@
-import React from 'react';
-import ProjectsLayout from '../components/ProjectsLayout';
-import Img from 'gatsby-image';
+import React from "react";
+import ProjectsLayout from "../components/ProjectsLayout";
+import Img from "gatsby-image";
 import * as styles from "../styles/project-details.module.css";
+import { graphql } from "gatsby";
 
-export default function ProjectDetails() {
+export default function ProjectDetails({ data }) {
+    const { html } = data.markdownRemark
+    const { title, stack, featuredImg } = data.markdownRemark.frontmatter
   return (
     <ProjectsLayout>
       <div className={styles.details}>
-        <h2>Title</h2>
-        <h3>stack</h3>
+        <h2>{title}</h2>
+        <h3>{stack}</h3>
         <div className={styles.featured}>
-            {/* <Img fluid={} /> */}
+            <Img fluid={featuredImg.childImageSharp.fluid} />
         </div>
-        {/* <div className={styles.html} dangerouslySetInnerHTML={} /> */}
+        <div className={styles.html} dangerouslySetInnerHTML={{ __html: html }} />
       </div>
     </ProjectsLayout>
   )
 }
+
+export const query = graphql`
+  query ProjectDetails ($slug: String) {
+    markdownRemark(frontmatter: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        stack
+        title
+        featuredImg {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
