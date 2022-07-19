17c387466d19b5c4ce61a51d74add9d31978d557
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2022-07-19 20:12:31 +0200

    Create new object type

--- index.js
+++ index.js
@@ -3,7 +3,14 @@ const { ApolloServer, gql } = require("apollo-server");
 
 const typeDefs = gql`
   type Query {
-    hello: [String!]!
+    products: [Product!]!
+  }
+  type Product {
+    name: String!
+    description: String!
+    quantity: Int!
+    price: Float!
+    onSale: Boolean!
   }
 `;
 
