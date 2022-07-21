166e07378a22c9fc2802e408d8380d048f18e76f
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2022-07-21 19:47:43 +0200

    Fix code misplacement

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -1,9 +1,36 @@
 import React from "react";
-import { graphql, staticQuery } from "gatsby";
+import { graphql, StaticQuery, Link } from "gatsby";
+// import Layout from "../components/layout"
 
-const MainMenu = () => (
+const MainMenu = ()=> (
+  <StaticQuery
+    query={graphql`
+      {
+        allWordpressWpApiMenusMenusItems(filter: {
+          name:{
+            eq: "Main menu"
+          }
+        }){
+          edges{
+            node{
+              name
+              items{
+                title
+                object_slug
+              }
+            }
+          }
+        }
+      }
+    `}
+    render={props => (
     <div>
-        Main Menu
+        {props.allWordpressWpApiMenusMenusItems.edges[0].node.items.map(item =>
+            <Link to={`/${item.object_slug}`} key={item.title}>
+                {item.title}
+            </Link>)}
     </div>
+    )}
+  />
 );
 export default MainMenu;
--- src/templates/page.js
+++ src/templates/page.js
@@ -1,36 +1,8 @@
 import React from "react";
-import { graphql, StaticQuery, Link } from "gatsby";
-// import Layout from "../components/layout"
+import Layout from "../components/layout"
 
-const MainMenu = ()=> (
-  <StaticQuery
-    query={graphql`
-      {
-        allWordpressWpApiMenusMenusItems(filter: {
-          name:{
-            eq: "Main menu"
-          }
-        }){
-          edges{
-            node{
-              name
-              items{
-                title
-                object_slug
-              }
-            }
-          }
-        }
-      }
-    `}
-    render={props => (
-    <div>
-        {props.allWordpressWpApiMenusMenusItems.edges[0].node.items.map(item =>
-            <Link to={`/${item.object_slug}`} key={item.title}>
-                {item.title}
-            </Link>)}
-    </div>
-    )}
-  />
+export default ({ pageContext }) => (
+    <Layout>
+        <h1 dangerouslySetInnerHTML={{__html: pageContext.title}} />
+    </Layout>
 );
-export default MainMenu;
