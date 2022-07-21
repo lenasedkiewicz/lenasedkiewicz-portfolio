bc23e28c958f4604d4191a6589b87b590b4b438e
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2022-07-21 19:41:23 +0200

    Add filter to menu query

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,8 +1,36 @@
 import React from "react";
-import Layout from "../components/layout";
+import { graphql, StaticQuery, Link } from "gatsby";
+// import Layout from "../components/layout"
 
-export default ({ pageContext }) => (
-    <Layout>
-        <h1 dangerouslySetInnerHTML={{__html: pageContext.title}} />
-    </Layout>
+const MainMenu = ()=> (
+  <StaticQuery
+    query={graphql`
+      {
+        allWordpressWpApiMenusMenusItems(filter: {
+          name:{
+            eq: "Main menu"
+          }
+        }){
+          edges{
+            node{
+              name
+              items{
+                title
+                object_slug
+              }
+            }
+          }
+        }
+      }
+    `}
+    render={props => (
+    <div>
+        {props.allWordpressWpApiMenusMenusItems.edges[0].node.items.map(item =>
+            <Link to={`/${item.object_slug}`} key={item.title}>
+                {item.title}
+            </Link>)}
+    </div>
+    )}
+  />
 );
+export default MainMenu;
