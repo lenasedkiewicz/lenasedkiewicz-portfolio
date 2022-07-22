e56683557bd478368475df70245f12067f2a2f24
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2022-07-22 18:24:11 +0200

    Add custom post type

--- functions.php
+++ functions.php
@@ -1,3 +1,15 @@
 <?php
 add_theme_support( 'custom-logo' );
 add_theme_support( 'menus' );
+
+function create_custom_portfolio_post_type(){
+    register_post_type('portfolio',
+                    array(
+                        'labels' => array(
+                            'name' => __('Portfolio'),
+                             'singular_name' => __('Portfolio')
+                        ),
+                        'show_in_admin_bar' => true,
+                        'show_in_rest' => true
+                    ));
+}
