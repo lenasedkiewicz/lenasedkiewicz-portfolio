11b48365c8b58fca2b61fc50df0709f3b612c7bb
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2022-07-26 15:33:30 +0200

    Attempt to fix an error

--- src/templates/blogPostList.js
+++ src/templates/blogPostList.js
@@ -1,8 +1,23 @@
-import React from "react";
-import Layout from "../components/layout";
+import React from "react"
+import Layout from "../components/layout"
+import { Link } from "gatsby"
 
-export default ({pageContext}) => (
-    <Layout>
-        Blog
-    </Layout>
+export default ({ pageContext }) => (
+  <Layout>
+    {pageContext.posts.map(post => (
+      <div key={post.node.wordpress_id}>
+        <h3 dangerouslySetInnerHTML={{ __html: post.node.title }} />
+        <p dangerouslySetInnerHTML={{ __html: post.node.excerpt }} />
+      </div>
+    ))}
+    {/* <Pagination> */}
+    {Array.from({ lenght: pageContext.numberOfPages }).map((page, index) => (
+      <div key={index}>
+        <Link to={index === 0 ? "/blog/" : `/blog/${index + 1}`}>
+          {index + 1}
+        </Link>
+      </div>
+    ))}
+    {/* </Pagination> */}
+  </Layout>
 )
